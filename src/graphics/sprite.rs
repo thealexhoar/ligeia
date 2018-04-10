24a8b0a15eacab905cb4ad0474e81a8826943201
Alex Hoar <thealexhoar@gmail.com>
2018-04-10 15:25:31 -0700

    Minor changes.

--- src/graphics/sprite.rs
+++ src/graphics/sprite.rs
@@ -1,3 +1,20 @@
+use sfml::graphics::{IntRect, Vertex};
+use graphics::TextureHandle;
 
+pub struct Sprite {
+    _vertices: [Vertex; 4],
+    _texture_handle: TextureHandle,
+    _texture_rect: IntRect
+}
 
-pub struct Sprite {}
+impl Sprite {
+    pub fn new(texture: TextureHandle) -> Self {
+        Self {
+
+        }
+    }
+
+    pub fn with_subrect(texture: TextureHandle, rect: IntRect) -> Self {
+        Self {}
+    }
+}
